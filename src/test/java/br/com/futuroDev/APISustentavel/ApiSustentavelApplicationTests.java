0b233880757fd442f754376c903045d7685f7c44
Franciane SL <fran.schier@outlook.com>
2025-04-17 21:00:43 -0300

    Meu primeiro commit da API Sustentavel

---
+++ src/test/java/br/com/futuroDev/APISustentavel/ApiSustentavelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.futuroDev.APISustentavel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSustentavelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
