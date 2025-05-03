248680238ac2b748c334d1576f347e2748fd2702
Franciane SL <fran.schier@outlook.com>
2025-05-02 21:31:32 -0300

    Implementação da classe UserDetailsServiceImpl

---
+++ src/main/java/br/com/futuroDev/APISustentavel/Service/UserDetailsServiceImpl.java
@@ -0,0 +1,33 @@
+package br.com.futuroDev.APISustentavel.Service;
+
+import br.com.futuroDev.APISustentavel.Model.Entity.User;
+import br.com.futuroDev.APISustentavel.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario não encontrado pelo username" + username));
+
+        Collection<GrantedAuthority> authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
+    }
+}
