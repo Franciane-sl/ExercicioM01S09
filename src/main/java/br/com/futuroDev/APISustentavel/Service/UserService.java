06f91beea55d4f4266e3e58626cf48546f1fa6c9
Franciane SL <fran.schier@outlook.com>
2025-05-03 20:44:28 -0300

    Implementação da classe UserService

---
+++ src/main/java/br/com/futuroDev/APISustentavel/Service/UserService.java
@@ -0,0 +1,41 @@
+package br.com.futuroDev.APISustentavel.Service;
+
+import br.com.futuroDev.APISustentavel.Model.Entity.User;
+import br.com.futuroDev.APISustentavel.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    UserDetailsServiceImpl userDetailsService;
+
+    public User findByUsername(String username) {
+        return userRepository.findByUserName(username)
+                .orElseThrow(()-> new RuntimeException("Usuário não encontrado."));
+    }
+
+    public boolean validatePassword(String rawPassword, String encodedPassword) {
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        return passwordEncoder.matches(rawPassword, encodedPassword);
+    }
+
+    public UserDetails loadUserByUsername(String username) {
+        return userDetailsService.loadUserByUsername(username);
+    }
+
+    public void registerUser(User user){
+        if (!userRepository.existsByUsername(user.getUsername())){
+            userRepository.save(user);
+        }else{
+            throw new RuntimeException("O usuário já exite.");
+        }
+    }
+}
