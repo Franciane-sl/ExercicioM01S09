49ca48eb2b7f728135afeffef5074eaa13718786
Franciane SL <fran.schier@outlook.com>
2025-05-04 19:19:50 -0300

    Modificação do nome da classe

--- src/main/java/br/com/futuroDev/APISustentavel/Configuration/JwtAuthenticationFilter.java
+++ src/main/java/br/com/futuroDev/APISustentavel/Configuration/JwtAuthenticationFilter.java
@@ -15,7 +15,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @Component
-public class JwtAutenticationFilter extends OncePerRequestFilter {
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Autowired
     JwtUtil jwtUtil;
